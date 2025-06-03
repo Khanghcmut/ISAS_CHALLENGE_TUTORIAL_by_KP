a65656a759606f52c0f0aa78fcead5c903626fd5
Khanghcmut <138753176+Khanghcmut@users.noreply.github.com>
2025-06-03 23:18:51 +0700

    init commit

---
+++ custom_features.py
@@ -0,0 +1,16 @@
+from tsfel.feature_extraction.features_utils import set_domain
+import numpy as np
+from scipy import stats
+@set_domain("domain", "Custom")
+def Mode(signal):
+    """
+    Calculate the mode (most frequent value) of the input signal.
+
+    Parameters:
+        signal (array-like): 1D array, list, or Series containing numerical data.
+
+    Returns:
+        float: Mode of the signal. If multiple modes, returns the smallest one.
+    """
+    mode_result = stats.mode(signal, keepdims=False)
+    return mode_result.mode
